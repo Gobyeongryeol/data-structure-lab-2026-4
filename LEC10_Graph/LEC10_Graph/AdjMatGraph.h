4f19c24a46dce59df48daa533634cca9f6ce872b
Gobyeongryeol <rhqudfuf1@naver.com>
2026-05-29 10:17:08 +0900

    2026.05.29

--- LEC10_Graph/LEC10_Graph/AdjMatGraph.h
+++ LEC10_Graph/LEC10_Graph/AdjMatGraph.h
@@ -1,96 +1,318 @@
 #pragma once
-#define MAX_VTXS  20
+
+#define MAX_VTXS 20
 #define INF 9999
+#define MAX_HEAP_NODE (MAX_VTXS * MAX_VTXS)
+
 #include <stdio.h>
+#include <iostream>
+
+using namespace std;
 
 class AdjMatGraph {
 protected:
-    int    size;
-    char   vertices[MAX_VTXS];
-    int    adj[MAX_VTXS][MAX_VTXS];
-public:
-    AdjMatGraph() { reset(); }
-    char getVertex(int i) { return vertices[i]; }
-    int getEdge(int i, int j) { return adj[i][j]; }
-    void setEdge(int i, int j, int val) { adj[i][j] = val; }
-    bool isEmpty() { return size == 0; }
-    bool isFull() { return size >= MAX_VTXS; }
+    int size;
+    char vertices[MAX_VTXS];
+    int adj[MAX_VTXS][MAX_VTXS];
 
-    // 그래프 초기화 ==> 공백 상태의 그래프
+public:
+    AdjMatGraph() {
+        reset();
+    }
+
+    char getVertex(int i) {
+        return vertices[i];
+    }
+
+    int getEdge(int i, int j) {
+        return adj[i][j];
+    }
+
+    void setEdge(int i, int j, int val) {
+        adj[i][j] = val;
+    }
+
+    bool isEmpty() {
+        return size == 0;
+    }
+
+    bool isFull() {
+        return size >= MAX_VTXS;
+    }
+
     void reset() {
         size = 0;
-        for (int i = 0; i < MAX_VTXS; i++)
-            for (int j = 0; j < MAX_VTXS; j++)
+
+        for (int i = 0; i < MAX_VTXS; i++) {
+            for (int j = 0; j < MAX_VTXS; j++) {
                 setEdge(i, j, 0);
-    }
-    void insertVertex(char name) {
-        if (!isFull()) vertices[size++] = name;
-        else printf("Error: 그래프 정점 개수 초과\n");
-    }
-    // 간선 삽입: 무방향 그래프의 경우임. (방향, 가중치 그래프에서는 수정)
-    void insertEdge(int u, int v) {
-        setEdge(u, v, 1);
-        setEdge(v, u, 1); // 방향 그래프에서는 삭제됨(<u,v>만 존재)
-    }
-    // 그래프 정보 출력 (화면이나 파일에 출력)
-    void display(FILE* fp = stdout) {
-        fprintf(fp, "%d\n", size);		// 정점의 개수 출력
-        for (int i = 0; i < size; i++) {		// 각 행의 정보 출력
-            fprintf(fp, "%c ", getVertex(i));	// 정점의 이름 출력
-            for (int j = 0; j < size; j++)	// 간선 정보 출력
-                fprintf(fp, " %3d", getEdge(i, j));
-            fprintf(fp, "\n");
-    }
+            }
+        }
     }
 
+    void insertVertex(char name) {
+        if (!isFull())
+            vertices[size++] = name;
+        else
+            printf("Error: 그래프 정점 개수 초과\n");
+    }
+
+    void insertEdge(int u, int v) {
+        setEdge(u, v, 1);
+        setEdge(v, u, 1);
+    }
+
+    void display(FILE* fp = stdout) {
+        fprintf(fp, "%d\n", size);
+
+        for (int i = 0; i < size; i++) {
+            fprintf(fp, "%c ", getVertex(i));
+
+            for (int j = 0; j < size; j++) {
+                if (getEdge(i, j) == INF)
+                    fprintf(fp, "  -");
+                else
+                    fprintf(fp, " %3d", getEdge(i, j));
+            }
+
+            fprintf(fp, "\n");
+        }
+    }
 };
-class SrchAMGraph : public AdjMatGraph
-{
-    bool visited[MAX_VTXS];	 // 정점의 방문 정보
+
+class SrchAMGraph : public AdjMatGraph {
+    bool visited[MAX_VTXS];
+
 public:
-    SrchAMGraph() { resetVisited(); }
-    void resetVisited() {	// 모든 정점을 방문하지 않았다고 설정
+    SrchAMGraph() {
+        resetVisited();
+    }
+
+    void resetVisited() {
         for (int i = 0; i < size; i++)
             visited[i] = false;
     }
-    bool isLinked(int u, int v) { return getEdge(u, v) != 0; }
 
-    // 깊이 우선 탐색 함수
+    bool isLinked(int u, int v) {
+        return getEdge(u, v) != 0;
+    }
+
     void DFS(int v) {
-        visited[v] = true;	 // 현재 정점을 방문함
-        printf("%c ", getVertex(v)); // 정점의 이름 출력
+        visited[v] = true;
+        printf("%c ", getVertex(v));
 
-        for (int w = 0; w < size; w++)
+        for (int w = 0; w < size; w++) {
             if (isLinked(v, w) && visited[w] == false)
-            DFS(w);	 // 연결 + 방문X => 순환호출로 방문
+                DFS(w);
+        }
     }
 };
 
 class WGraph : public AdjMatGraph {
 public:
     void insertEdge(int u, int v, int weight) {
-        if (weight > INF) weight = INF;
+        if (weight > INF)
+            weight = INF;
+
         setEdge(u, v, weight);
     }
-    bool hasEdge(int i, int j) { return (getEdge(i, j) < INF); }
+
+    bool hasEdge(int i, int j) {
+        return i != j && getEdge(i, j) < INF;
+    }
 
     void load(const char* filename) {
         FILE* fp;
         fopen_s(&fp, filename, "r");
-        if (fp != NULL) {
-            int n, val;
-            fscanf_s(fp, "%d", &n); // 정점의 전체 개수
-            for (int i = 0; i < n; i++) {
-                char str[80];
-                int val;
-                fscanf_s(fp, "%s", str, sizeof(str)); // 정점의 이름
-                insertVertex(str[0]); // 정점 삽입
-                for (int j = 0; j < n; j++) {
-                    fscanf_s(fp, "%d", &val); // 간선 정보
-                    insertEdge(i, j, val); // 간선 삽입
-                }
+
+        if (fp == NULL) {
+            printf("파일을 열 수 없습니다: %s\n", filename);
+            return;
+        }
+
+        int n;
+        fscanf_s(fp, "%d", &n);
+
+        for (int i = 0; i < n; i++) {
+            char str[80];
+            int val;
+
+            fscanf_s(fp, "%s", str, (unsigned)_countof(str));
+
+            insertVertex(str[0]);
+
+            for (int j = 0; j < n; j++) {
+                fscanf_s(fp, "%d", &val);
+
+                if (i != j && val == 0)
+                    val = INF;
+
+                insertEdge(i, j, val);
             }
         }
+
         fclose(fp);
+    }
+};
+
+class VertexSets {
+    int parent[MAX_VTXS];
+    int nSets;
+
+public:
+    VertexSets(int n) : nSets(n) {
+        for (int i = 0; i < nSets; i++)
+            parent[i] = -1;
+    }
+
+    bool isRoot(int i) {
+        return parent[i] < 0;
+    }
+
+    int findSet(int v) {
+        while (!isRoot(v))
+            v = parent[v];
+
+        return v;
+    }
+
+    void unionSets(int s1, int s2) {
+        parent[s1] = s2;
+        nSets--;
+    }
+};
+
+class HeapNode {
+private:
+    int key;
+    int v1;
+    int v2;
+
+public:
+    HeapNode() : key(0), v1(-1), v2(-1) {
+    }
+
+    HeapNode(int k, int u, int v)
+        : key(k), v1(u), v2(v) {
+    }
+
+    void setKey(int k, int u, int v) {
+        key = k;
+        v1 = u;
+        v2 = v;
+    }
+
+    int getKey() {
+        return key;
+    }
+
+    int getV1() {
+        return v1;
+    }
+
+    int getV2() {
+        return v2;
+    }
+};
+
+class MinHeap {
+private:
+    HeapNode node[MAX_HEAP_NODE];
+    int size;
+
+public:
+    MinHeap() : size(0) {
+    }
+
+    bool isEmpty() {
+        return size == 0;
+    }
+
+    bool isFull() {
+        return size >= MAX_HEAP_NODE - 1;
+    }
+
+    HeapNode& getParent(int i) {
+        return node[i / 2];
+    }
+
+    void insert(int key, int u, int v) {
+        if (isFull()) {
+            cout << "Heap is full." << endl;
+            return;
+        }
+
+        int i = ++size;
+
+        while (i != 1 && key < getParent(i).getKey()) {
+            node[i] = getParent(i);
+            i /= 2;
+        }
+
+        node[i].setKey(key, u, v);
+    }
+
+    HeapNode remove() {
+        if (isEmpty()) {
+            cout << "Heap is empty." << endl;
+            return HeapNode();
+        }
+
+        HeapNode item = node[1];
+        HeapNode last = node[size--];
+
+        int parent = 1;
+        int child = 2;
+
+        while (child <= size) {
+            if (child < size && node[child].getKey() > node[child + 1].getKey())
+                child++;
+
+            if (last.getKey() <= node[child].getKey())
+                break;
+
+            node[parent] = node[child];
+
+            parent = child;
+            child *= 2;
+        }
+
+        node[parent] = last;
+
+        return item;
+    }
+};
+
+class WGraphMST : public WGraph {
+public:
+    void Kruskal() {
+        MinHeap heap;
+
+        for (int i = 0; i < size - 1; i++) {
+            for (int j = i + 1; j < size; j++) {
+                if (hasEdge(i, j))
+                    heap.insert(getEdge(i, j), i, j);
+            }
+        }
+
+        VertexSets set(size);
+        int edgeAccepted = 0;
+
+        while (edgeAccepted < size - 1 && !heap.isEmpty()) {
+            HeapNode e = heap.remove();
+
+            int uset = set.findSet(e.getV1());
+            int vset = set.findSet(e.getV2());
+
+            if (uset != vset) {
+                printf("간선 추가 : %c - %c (비용:%d)\n",
+                    getVertex(e.getV1()),
+                    getVertex(e.getV2()),
+                    e.getKey());
+
+                set.unionSets(uset, vset);
+                edgeAccepted++;
+            }
+        }
     }
 };
